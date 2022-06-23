161b266255ee94c4089f7919c92bff2eebee574c
Kil4E04 <kilianu66@gmail.com>
2022-06-23 11:39:39 +0200

    nla

--- Barcode_Scanner/src/app/scanner/scanner.component.ts
+++ Barcode_Scanner/src/app/scanner/scanner.component.ts
@@ -4,18 +4,33 @@ import { BarcodeScannerLivestreamComponent } from 'ngx-barcode-scanner';
 
 @Component({
   selector: 'app-scanner',
-  templateUrl: './scanner.component.html',
+  template: `
+    <barcode-scanner-livestream
+      maxHeight="200px"
+      maxWidth="350px"
+      type="code_128"
+      (valueChanges)="onValueChanges($event)"
+      (started)="(onStarted)"
+    ></barcode-scanner-livestream>
+    <div [hidden]="!barcodeValue">
+      {{ barcodeValue }}
+    </div>
+  `,
   styleUrls: ['./scanner.component.css']
 })
 export class ScannerComponent implements AfterViewInit {
 
-  @ViewChild('barcodeScanner')
+  @ViewChild(BarcodeScannerLivestreamComponent)
   barcodeScanner!: BarcodeScannerLivestreamComponent;
 
   barcodeValue: any;
 
+  
+
   ngAfterViewInit(){
     this.barcodeScanner.start();
+    console.log('Started')
+    console.log(this.barcodeScanner.isStarted)
   }
 
   onValueChanges(result: any){
